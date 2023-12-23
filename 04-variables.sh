895d66b5568c7b3eec56e11836af9861113f21eb
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2023-12-23 10:37:26 +0530

    Delete 04-variables.sh

--- 04-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=$1
-PERSON2=$2
-
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
